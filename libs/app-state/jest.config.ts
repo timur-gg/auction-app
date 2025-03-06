735e42869af953c272b755fa374be26fd1d932e0
arturShevts <arturshevtsov97@gmail.com>
2025-03-06 18:26:36 -0500

    add lib for app-state

--- libs/app-state/jest.config.ts
+++ libs/app-state/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
   displayName: 'app-state',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../coverage/app-state',
+  coverageDirectory: '../../coverage/libs/app-state',
 };
--- libs/app-state/vite.config.ts
+++ libs/app-state/vite.config.ts
@@ -8,7 +8,7 @@ import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../node_modules/.vite/app-state',
+  cacheDir: '../../node_modules/.vite/libs/app-state',
   plugins: [
     react(),
     nxViteTsPaths(),
@@ -25,7 +25,7 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir: '../dist/app-state',
+    outDir: '../../dist/libs/app-state',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {

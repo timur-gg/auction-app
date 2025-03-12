9dfca6654f113cb58033f21d3546b131d710b5a4
arturShevts <arturshevtsov97@gmail.com>
2025-03-12 13:38:33 -0400

    update api

---
+++ apps/rest-api/src/common/config/database.config.ts
@@ -0,0 +1,17 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => {
+  return {
+     host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
+    autoLoadEntities: true,
+    type: 'postgres',
+    entities: [__dirname + '/../**/*.entity.{js,ts}'], //<- doesnt work!!!
+    database: 'auction_db',
+    synchronize: true,
+    logging: true,
+  };
+});

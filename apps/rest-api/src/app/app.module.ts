63d77cb00be22098fe61e9aacb7eeff526e59583
arturShevts <arturshevtsov97@gmail.com>
2025-03-11 21:32:58 -0400

    add psql support

--- apps/rest-api/src/app/app.module.ts
+++ apps/rest-api/src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    password: 'nest',
+    username: 'nest',
+    entities: [],
+    database: 'auction_db',
+    synchronize: true,
+    logging: true,
+  }),
+    // UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

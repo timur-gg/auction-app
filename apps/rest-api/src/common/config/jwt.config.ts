d2b5251f82d9c8edc4c2977aec543902bdd42a7a
timur-gg <avanteggarde@gmail.com>
2025-03-22 22:10:22 -0400

    fix: token expiry date fix: postgres users migration

--- apps/rest-api/src/common/config/jwt.config.ts
+++ apps/rest-api/src/common/config/jwt.config.ts
@@ -1,8 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('jwt', () => {
+
   return {
     secret: process.env.JWT_SECRET,
-    accessTokenTtl: process.env.JWT_ACCESS_TOKEN_TTL,
+    accessTokenTtl: Number(process.env.JWT_ACCESS_TOKEN_TTL),
   };
 });
